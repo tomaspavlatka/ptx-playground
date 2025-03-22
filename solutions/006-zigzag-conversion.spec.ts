e7d0d621a1738b978551966a5219c4c1029cbd3a
Tomas Moya Pavlatka <tomas@pavlatka.cz>
2025-03-22 09:55:20 +0100

    feat: added improved version for zigzag

--- solutions/006-zigzag-conversion.spec.ts
+++ solutions/006-zigzag-conversion.spec.ts
@@ -1,43 +1,87 @@
-import { convert } from "./006-zigzag-conversion";
+import { convert, convertImproved } from "./006-zigzag-conversion";
 
 describe('006-zigzag-conversion', () => {
-  it('scenario 1', () => {
-    const input = '';
+  describe('brute-force', () => {
+    it('scenario 1', () => {
+      const input = '';
 
-    const response = convert(input, 1);
+      const response = convert(input, 1);
 
-    expect(response).toBe('');
+      expect(response).toBe('');
+    });
+
+    it('scenario 2', () => {
+      const input = 'PAYPALISHIRING';
+
+      const response = convert(input, 3);
+
+      expect(response).toBe('PAHNAPLSIIGYIR');
+    });
+
+    it('scenario 3', () => {
+      const input = 'PAYPALISHIRING';
+
+      const response = convert(input, 4);
+
+      expect(response).toBe('PINALSIGYAHRPI');
+    });
+
+    it('scenario 4', () => {
+      const input = 'AB';
+
+      const response = convert(input, 1);
+
+      expect(response).toBe('AB');
+    });
+
+    it('scenario 5', () => {
+      const input = 'ABCD';
+
+      const response = convert(input, 2);
+
+      expect(response).toBe('ACBD');
+    });
   });
 
-  it('scenario 2', () => {
-    const input = 'PAYPALISHIRING';
+  describe('improved', () => {
+    it('scenario 1', () => {
+      const input = '';
 
-    const response = convert(input, 3);
+      const response = convertImproved(input, 1);
 
-    expect(response).toBe('PAHNAPLSIIGYIR');
-  });
+      expect(response).toBe('');
+    });
 
-  it('scenario 3', () => {
-    const input = 'PAYPALISHIRING';
+    it('scenario 2', () => {
+      const input = 'PAYPALISHIRING';
 
-    const response = convert(input, 4);
+      const response = convertImproved(input, 3);
 
-    expect(response).toBe('PINALSIGYAHRPI');
-  });
+      expect(response).toBe('PAHNAPLSIIGYIR');
+    });
 
-  it('scenario 4', () => {
-    const input = 'AB';
+    it('scenario 3', () => {
+      const input = 'PAYPALISHIRING';
 
-    const response = convert(input, 1);
+      const response = convertImproved(input, 4);
 
-    expect(response).toBe('AB');
-  });
+      expect(response).toBe('PINALSIGYAHRPI');
+    });
 
-  it('scenario 5', () => {
-    const input = 'ABCD';
+    it('scenario 4', () => {
+      const input = 'AB';
 
-    const response = convert(input, 2);
+      const response = convertImproved(input, 1);
 
-    expect(response).toBe('ACBD');
+      expect(response).toBe('AB');
+    });
+
+    it('scenario 5', () => {
+      const input = 'ABCD';
+
+      const response = convertImproved(input, 2);
+
+      expect(response).toBe('ACBD');
+    });
   });
 });
--- solutions/006-zigzag-conversion.ts
+++ solutions/006-zigzag-conversion.ts
@@ -61,3 +61,27 @@ export function convert(s: string, numRows: number): string {
   return rows.join('');
 };
 
+export function convertImproved(s: string, numRows: number): string {
+  if (numRows < 2) {
+    return s;
+  }
+
+  let rows = new Array(numRows).fill('');
+
+  let currentRow = -1;
+  let ascending = true;
+
+  for (let i = 0; i < s.length; i++) {
+    currentRow += ascending ? 1 : -1;
+    rows[currentRow] += s[i];
+
+    if (currentRow === numRows - 1) {
+      ascending = false;
+    } else if (currentRow === 0) {
+      ascending = true;
+    }
+  }
+
+  return rows.join('');
+};
+
